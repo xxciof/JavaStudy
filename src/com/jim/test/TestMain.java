e131fb1de3985699ec107ea774e9924018949fe5
JIM CHEN <jim20120805@hotmail.com>
2017-02-14 12:10:54 +0800

    first commit

---
+++ src/com/jim/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.jim.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("LEARNING GIT...");
+	}
+
+}
